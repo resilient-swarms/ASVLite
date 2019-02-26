4fb7d798d13ccc28c90df9238385f8bbeef8e21e
Toby Thomas <toby@tobythomas.tech>
2019-02-26 19:05:13 +0000

    Minor modifications and comments added.

--- source/main.cpp
+++ source/main.cpp
@@ -4,15 +4,17 @@ using namespace asv_swarm;
 
 int main()
 {
-  /* Define sea condition */
+  /* Create object to coordinate visualization */
+  Visualization visualization;
+
+  /* Set the sea condition */
   Quantity<Units::length> wind_fetch {100*Units::kilo*Units::meter};
   Quantity<Units::velocity> wind_speed {15*Units::meter_per_second};
-  /* wind direction is 30deg east of north*/
   Quantity<Units::plane_angle> wind_direction {Const::PI/6 * Units::radian};
 
-  /* Add observer to timer event */
-  Visualization visualization;
   visualization.set_sea_condition(wind_speed, wind_fetch, wind_direction);
+
+  /* Start visualization */
   visualization.start();
 
   return EXIT_SUCCESS;
