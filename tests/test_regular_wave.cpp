307ee3fbac96de9223888bfe2d1156d212b402c0
Toby Thomas <toby@tobythomas.tech>
2019-03-05 18:00:36 +0000

    Cleaned CMakeList. Modified CMakeList for unittesting. Modified test_regular_wave.cpp to include namespace correction.

--- tests/test_regular_wave.cpp
+++ tests/test_regular_wave.cpp
@@ -3,6 +3,7 @@
 #include "exception.h"
 
 using namespace asv_swarm;
+using namespace asv_swarm::Hydrodynamics;
 
 TEST(regular_wave, check_constructor)
 {
@@ -10,12 +11,12 @@ TEST(regular_wave, check_constructor)
   Quantity<Units::length> amplitude{2.5*Units::meter};
   Quantity<Units::frequency> frequency{0.1*Units::hertz};
   Quantity<Units::plane_angle> direction{0.0*Units::radian};
-  Quantity<Units::plane_angle> phase{Const::PI*Units::radian};
+  Quantity<Units::plane_angle> phase{Constant::PI*Units::radian};
   Regular_wave wave{amplitude, frequency, direction, phase};
   EXPECT_EQ(2.5, wave.get_wave_amplitude().value());
   EXPECT_EQ(0.1, wave.get_wave_frequency().value());
   EXPECT_EQ(0.0, wave.get_direction().value());
-  EXPECT_EQ(Const::PI, wave.get_phase().value());
+  EXPECT_EQ(Constant::PI, wave.get_phase().value());
 }
 
 TEST(regular_wave, check_wave_properties)
@@ -24,7 +25,7 @@ TEST(regular_wave, check_wave_properties)
   Quantity<Units::length> amplitude{2.5*Units::meter};
   Quantity<Units::frequency> frequency{0.1*Units::hertz};
   Quantity<Units::plane_angle> direction{0.0*Units::radian};
-  Quantity<Units::plane_angle> phase{Const::PI*Units::radian};
+  Quantity<Units::plane_angle> phase{Constant::PI*Units::radian};
   Regular_wave wave{amplitude, frequency, direction, phase};
   EXPECT_NEAR(6163.804, wave.get_wave_length().value(), 0.001);
   EXPECT_NEAR(0.00101, wave.get_wave_number().value(), 0.00001);
@@ -36,8 +37,8 @@ TEST(regular_wave, check_wave_elevation)
   //Check the wave elevation.
   Quantity<Units::length> amplitude{2.5*Units::meter};
   Quantity<Units::frequency> frequency{0.1*Units::hertz};
-  Quantity<Units::plane_angle> direction{(Const::PI/4)*Units::radian};
-  Quantity<Units::plane_angle> phase{(Const::PI/6)*Units::radian};
+  Quantity<Units::plane_angle> direction{(Constant::PI/4)*Units::radian};
+  Quantity<Units::plane_angle> phase{(Constant::PI/6)*Units::radian};
   Regular_wave wave{amplitude, frequency, direction, phase};
   EXPECT_NEAR(2.4361, 
               wave.get_wave_elevation(1.5 * Units::meter,
@@ -51,10 +52,10 @@ TEST(regular_wave, check_exception_invalid_amplitude)
   //Check the wave elevation.
   Quantity<Units::length> amplitude{0.0*Units::meter};
   Quantity<Units::frequency> frequency{0.1*Units::hertz};
-  Quantity<Units::plane_angle> direction{(Const::PI/4)*Units::radian};
-  Quantity<Units::plane_angle> phase{(Const::PI/6)*Units::radian};
+  Quantity<Units::plane_angle> direction{(Constant::PI/4)*Units::radian};
+  Quantity<Units::plane_angle> phase{(Constant::PI/6)*Units::radian};
   EXPECT_THROW(Regular_wave(amplitude, frequency, direction, phase), 
-               ValueError);  
+               Exception::ValueError);  
 }
 
 TEST(regular_wave, check_exception_invalid_frequency)
@@ -62,10 +63,10 @@ TEST(regular_wave, check_exception_invalid_frequency)
   //Check the wave elevation.
   Quantity<Units::length> amplitude{2.5*Units::meter};
   Quantity<Units::frequency> frequency{0.0*Units::hertz};
-  Quantity<Units::plane_angle> direction{(Const::PI/4)*Units::radian};
-  Quantity<Units::plane_angle> phase{(Const::PI/6)*Units::radian};
+  Quantity<Units::plane_angle> direction{(Constant::PI/4)*Units::radian};
+  Quantity<Units::plane_angle> phase{(Constant::PI/6)*Units::radian};
   EXPECT_THROW(Regular_wave(amplitude, frequency, direction, phase), 
-               ValueError);  
+               Exception::ValueError);  
 }
 
 
