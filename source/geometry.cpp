a7fd25138450557e66c48a520f4d48c6ed7aa8d0
Toby Thomas <toby@tobythomas.tech>
2019-03-12 22:22:14 +0000

    Added default constructor.

--- source/geometry.cpp
+++ source/geometry.cpp
@@ -3,6 +3,12 @@
 using namespace asv_swarm;
 using namespace asv_swarm::Geometry;
 
+Point::Point():
+  x{0.0*Units::meter},
+  y{0.0*Units::meter},
+  z{0.0*Units::meter}
+{}
+
 Point::Point(Quantity<Units::length> x,
              Quantity<Units::length> y,
              Quantity<Units::length> z):
@@ -10,3 +16,5 @@ Point::Point(Quantity<Units::length> x,
   y{y},
   z{z}
 {}
+
+
