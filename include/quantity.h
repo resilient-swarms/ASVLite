2a3eef185d821228fa4c9a5fdb3e066d79c078d0
Toby Thomas <toby@tobythomas.tech>
2019-03-19 18:52:52 +0000

    New quantity and units defined for density.

--- include/quantity.h
+++ include/quantity.h
@@ -28,14 +28,25 @@ typedef derived_dimension<mass_base_dimension,1,
                         time_base_dimension,-2>::type stiffness_coeff_dimension;
 
 
+/**
+ * Derived dimension for density.
+ */
+typedef derived_dimension<mass_base_dimension,1,
+                          length_base_dimension,-3>::type density_dimension;
+
+
 
 namespace si 
 {
 typedef unit<damping_coeff_dimension, si::system> damping_coefficient;
-typedef unit<stiffness_coeff_dimension, si::system> stiffness_coefficient;
+BOOST_UNITS_STATIC_CONSTANT(newton_sec_per_meter, damping_coefficient);
 
-BOOST_UNITS_STATIC_CONSTANT(newton_per_meter_per_sec, damping_coefficient);
+typedef unit<stiffness_coeff_dimension, si::system> stiffness_coefficient;
 BOOST_UNITS_STATIC_CONSTANT(newton_per_meter, stiffness_coefficient);
+
+typedef unit<density_dimension, si::system> density;
+BOOST_UNITS_STATIC_CONSTANT(kilogram_per_meter_cube, density);
+
 
 } // namespace si
 } // namespace units
