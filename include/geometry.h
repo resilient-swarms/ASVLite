77c393c01e7ce45e99439bd5a34a33d8d264daf1
Toby Thomas <toby@tobythomas.tech>
2019-04-29 11:01:01 +0100

    Class Attitude for representing the orientation of ASV with global coordinates.

--- include/geometry.h
+++ include/geometry.h
@@ -17,6 +17,9 @@ public:
 
   /**
    * Construct with coordinates provided.
+   * @param x is the distance along the x axis in meter.
+   * @param y is the distance along the y axis in meter.
+   * @param z is teh distance along the z axis in meter.
    */
   Point(Quantity<Units::length> x,
         Quantity<Units::length> y,
@@ -26,6 +29,33 @@ public:
   Quantity<Units::length> y;
   Quantity<Units::length> z;
 }; // class Point
+
+/**
+ * Class to represent the orientation of the vessel with respect to x, y, z
+ * coordinates.
+ */
+class Attitude
+{
+public:
+  /**
+   * Default consturctor.
+   */
+  Attitude();
+
+  /**
+   * Constructor with angles to coordinate axis provided.
+   * @param x is the angle in radians with respect to x axis.
+   * @param y is the angle in radians with respect to y axis.
+   * @param z is the angle in radians with respect to z axis.
+   */
+  Attitude(Quantity<Units::plane_angle> x,
+           Quantity<Units::plane_angle> y,
+           Quantity<Units::plane_angle> z);
+
+  Quantity<Units::plane_angle> x;
+  Quantity<Units::plane_angle> y;
+  Quantity<Units::plane_angle> z;
+}; // class Attitude
 
 } // namespace Geometry
 } // namespace asv_swarm
--- source/geometry.cpp
+++ source/geometry.cpp
@@ -17,4 +17,17 @@ Point::Point(Quantity<Units::length> x,
   z{z}
 {}
 
+Attitude::Attitude():
+  x{0.0*Units::radian},
+  y{0.0*Units::radian},
+  z{0.0*Units::radian}
+{}
+
+Attitude::Attitude(Quantity<Units::plane_angle> x,
+                   Quantity<Units::plane_angle> y,
+                   Quantity<Units::plane_angle> z):
+  x{x},
+  y{y},
+  z{z}
+{}
 
