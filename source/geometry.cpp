57c7c19142650bf49e0fadc1a23d730bacdee051
Toby Thomas <toby@tobythomas.tech>
2019-05-01 12:00:29 +0100

    Renamed class Attitude to Orientation.

--- source/geometry.cpp
+++ source/geometry.cpp
@@ -17,13 +17,13 @@ Point::Point(Quantity<Units::length> x,
   z{z}
 {}
 
-Attitude::Attitude():
+Orientation::Orientation():
   x{0.0*Units::radian},
   y{0.0*Units::radian},
   z{0.0*Units::radian}
 {}
 
-Attitude::Attitude(Quantity<Units::plane_angle> x,
+Orientation::Orientation(Quantity<Units::plane_angle> x,
                    Quantity<Units::plane_angle> y,
                    Quantity<Units::plane_angle> z):
   x{x},
