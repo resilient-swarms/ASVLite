e547d34f67b2cef097eacdb808952063388b17db
Toby Thomas <toby@tobythomas.tech>
2019-05-11 00:30:19 +0100

    Constants used in the project.

---
+++ include/constants.h
@@ -0,0 +1,10 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <math.h>
+
+const double PI = M_PI;
+const double G = 9.81; // acceleration due to gravity in m/s2
+const double SEA_WATER_DENSITY = 1025; // density of sea water in Kg/m3
+
+#endif // CONSTANTS_H
