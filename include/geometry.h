5cb056fcc66461aa951f5b3beb40a41433ffc1f8
Toby Thomas <toby@tobythomas.tech>
2019-05-11 00:31:28 +0100

    Geometry new implementation as struct.

--- include/geometry.h
+++ include/geometry.h
@@ -1,63 +1,25 @@
 #ifndef GEOMETRY_H
 #define GEOMETRY_H
 
-#include "units_and_constants.h"
-
-namespace asv_swarm
+/**
+ * Structure to represent a point in 3D space.
+ */
+struct Point
 {
-namespace Geometry
-{
-class Point
-{
-public:
-  /**
-   * Default constructor.
-   */
-  Point();
-
-  /**
-   * Construct with coordinates provided.
-   * @param x is the distance along the x axis in meter.
-   * @param y is the distance along the y axis in meter.
-   * @param z is teh distance along the z axis in meter.
-   */
-  Point(Quantity<Units::length> x,
-        Quantity<Units::length> y,
-        Quantity<Units::length> z);
-
-  Quantity<Units::length> x;
-  Quantity<Units::length> y;
-  Quantity<Units::length> z;
-}; // class Point
+  double x;
+  double y;
+  double z;
+};
 
 /**
- * Class to represent the orientation in three dimensional space with respect to 
- * x, y, z coordinates.
+ * Class to represent the orientation in 3D space with respect to x, y, z 
+ * coordinates.
  */
-class Orientation
+struct Orientation
 {
-public:
-  /**
-   * Default constructor.
-   */
-  Orientation();
-
-  /**
-   * Constructor with angles to coordinate axis provided.
-   * @param x is the angle in radians with respect to x axis.
-   * @param y is the angle in radians with respect to y axis.
-   * @param z is the angle in radians with respect to z axis.
-   */
-  Orientation(Quantity<Units::plane_angle> x,
-           Quantity<Units::plane_angle> y,
-           Quantity<Units::plane_angle> z);
-
-  Quantity<Units::plane_angle> x;
-  Quantity<Units::plane_angle> y;
-  Quantity<Units::plane_angle> z;
-}; // class Attitude
-
-} // namespace Geometry
-} // namespace asv_swarm
+  double x;
+  double y;
+  double z;
+}; 
 
 #endif
