2f6476c290ee63a2cf08b5e45fc1c88c31a37a78
Toby Thomas <toby@tobythomas.tech>
2019-05-11 00:29:23 +0100

    Deleted all previous files. New start.

--- include/regular_wave.h
+++ include/regular_wave.h
@@ -1,102 +1,46 @@
-/**
- * This header file contains programming interface for defining regular ocean
- * waves.
- */
 #ifndef REGULAR_WAVE_H
 #define REGULAR_WAVE_H
 
-#include "units_and_constants.h"
+#include "geometry.h"
 
-namespace asv_swarm
-{
-namespace Hydrodynamics
-{
 /**
- * This class defines a regular sinusoidal wave. 
+ * Structure to defines a regular sinusoidal wave. 
  */
-class Regular_wave
+struct Regular_wave
 {
-public:
-  /**
-   * Constructor for Regular_wave.
-   * @param amplitude of wave in meters. Value should be greater than 0.0.
-   * @param frequency of wave in Hertz. Value should be greater than 0.0.
-   * @param direction of wave heading in radians with respect to global
-   * coordinate system's North direction (ie: positive Y direction). The 
-   * direction of angle measurement should be such that the East is at PI/2 
-   * radians to North (ie: clockwise direction measured positive). 
-   * @param phase angle of the wave in radian.
-   */
-  Regular_wave( Quantity<Units::length> amplitude, 
-                Quantity<Units::frequency> frequency, 
-                Quantity<Units::plane_angle> direction,
-                Quantity<Units::plane_angle> phase);
+  double amplitude; /* Amplitude of the wave in meter. */
+  double frequency; /* Frequency of the wave in Hz. */
+  double phase; /* Phase angle of the wave in radian. */
+  double direction; /* Direction of propagation of the wave with respect to 
+                     * geographic north. Angles are measured positive in 
+                     * clockwise direction. 
+                     */
+}; 
 
-  /**
-   * Method to get the wave length.
-   * @return wave length as object of type boost::units::length.
-   */
-  Quantity<Units::length> get_wave_length(){return wave_length;}
+/**
+ * Get the time period of the regular wave.
+ * @return time period in seconds.
+ */
+double regular_wave_get_time_period(struct Regular_wave* wave);
 
-  /**
-   * Method to get wave number.
-   * @return wave number as object of type boost::units::wavenumber.
-   */
-  Quantity<Units::wavenumber> get_wave_number(){return wave_number;}
+/**
+ * Get the wave length of a regular wave.
+ * @return wave length in meter.
+ */
+double regular_wave_get_length(struct Regular_wave* wave);
 
-  /**
-   * Method to get wave period.
-   * @return wave period as object of type boost::units::time.
-   */
-  Quantity<Units::time> get_wave_period(){return wave_period;}
+/**
+ * Get the wave number of a regular wave.
+ * @return wave number.
+ */
+double regular_wave_get_wave_number(struct Regular_wave* wave);
 
-  /**
-   * Method to get wave amplitude.
-   * @return wave amplitude as object of type boost::units::length.
-   */
-  Quantity<Units::length> get_wave_amplitude(){return amplitude;}
-
-  /**
-   * Method to get wave frequency.
-   * @return wave frequency as object of type boost::units::frequency.
-   */
-  Quantity<Units::frequency> get_wave_frequency(){return frequency;}
-
-  /**
-   * Method to get wave phase. The angle is measured positive clockwise.
-   * @return wave phase as object of type boost::units::plane_angle.
-   */
-  Quantity<Units::plane_angle> get_phase(){return phase;}
-
-  /**
-   * Method to get wave heading direction.
-   * @return wave direction as object of type boost::units::plane_angle.
-   */
-  Quantity<Units::plane_angle> get_direction(){return direction;}
-
-  /**
-   * Method to get wave elevation at a given location, at a given time.
-   * @param x coordinate of the location.
-   * @param y coordinate of the location.
-   * @param t time measured in seconds since start of simulation.
-   * @return the wave elevation as object of type boost::units::length.
-   */
-  Quantity<Units::length> get_wave_elevation(Quantity<Units::length> x,
-                                             Quantity<Units::length> y,
-                                             Quantity<Units::time> t);
-
-private:
-  Quantity<Units::length> amplitude; /* Amplitude of the wave. */
-  Quantity<Units::frequency> frequency; /* Frequency of the wave in Hz. */
-  Quantity<Units::plane_angle> phase; /* Phase angle of the wave in radian. */
-  Quantity<Units::plane_angle> direction; /* Direction of propagation of the 
-  wave with respect to y-axis. Angle measured positive clockwise. */
-  Quantity<Units::length> wave_length; /* Wave length in meter. */
-  Quantity<Units::wavenumber> wave_number; /* Wave number */
-  Quantity<Units::time> wave_period; /* Wave period in sec. */
-}; // class Regular_wave
-
-} // namespace Hydrodynamics 
-} // namespace asv_swarm
+/**
+ * Get wave elevation a given point for a given time.
+ * @param point at which the elevation is to be found with all coordinates in m.
+ * @param time at which elevation is to be found with time measured as seconds
+ * from the start of simulation.
+ */
+double regular_wave_get_elevation(struct Point* point, double time);
 
 #endif // REGULAR_WAVE_H
