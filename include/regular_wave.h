b3a5651cca0571a40c93da099775d9fbc84fd487
Toby Thomas <toby@tobythomas.tech>
2019-05-12 12:18:15 +0100

    Completed implementation of regular wave.

--- include/regular_wave.h
+++ include/regular_wave.h
@@ -8,39 +8,47 @@
  */
 struct Regular_wave
 {
-  double amplitude; /* Amplitude of the wave in meter. */
-  double frequency; /* Frequency of the wave in Hz. */
-  double phase; /* Phase angle of the wave in radian. */
-  double direction; /* Direction of propagation of the wave with respect to 
-                     * geographic north. Angles are measured positive in 
-                     * clockwise direction. 
-                     */
+  double amplitude; // Amplitude of the wave in meter.
+  double frequency; // Frequency of the wave in Hz.
+  double phase;     // Phase angle of the wave in radian.
+  double direction; // Direction of propagation of the wave with respect to 
+                    // geographic north. Angle are measured positive in 
+                    // clockwise direction such that east is at PI/2 radians to
+                    // north.
+  double time_period; // Time period of the wave in seconds.
+  double wave_length; // Wave length in meter.
+  double wave_number; // Wave number. Dimension less.
 }; 
 
 /**
- * Get the time period of the regular wave.
- * @return time period in seconds.
+ * Initialise a regular wave.
+ * @param wave is the pointer to the wave to be initialised. The function
+ * assumes that wave is not a null pointer and that all the values in the
+ * structure are to be overwritten.
+ * @param amplitude of the wave in meter. Assumes wave length is non-zero
+ * positive value.
+ * @param frequency of the wave in Hz. Assumes wave frequency is non-zero
+ * positive value.
+ * @param phase of the wave in radians.
+ * @param direction of propagation of the wave in radians with respect to the
+ * geographic north. Angle measured positive in clockwise direction such that 
+ * the east is at PI/2 radians to north.
  */
-double regular_wave_get_time_period(struct Regular_wave* wave);
-
-/**
- * Get the wave length of a regular wave.
- * @return wave length in meter.
- */
-double regular_wave_get_length(struct Regular_wave* wave);
-
-/**
- * Get the wave number of a regular wave.
- * @return wave number.
- */
-double regular_wave_get_wave_number(struct Regular_wave* wave);
-
+void regular_wave_init(struct Regular_wave* wave, 
+                         double amplitude, 
+                         double frequency, 
+                         double phase, 
+                         double direction);
 /**
  * Get wave elevation a given point for a given time.
- * @param point at which the elevation is to be found with all coordinates in m.
- * @param time at which elevation is to be found with time measured as seconds
- * from the start of simulation.
+ * @param wave for which the elevation is to be calculated.
+ * @param location at which the elevation is to be computed.
+ * @param time for which elevation is to be computed. Time is measured in 
+ * seconds from the start of simulation.
+ * @return wave elevation in meter.
  */
-double regular_wave_get_elevation(struct Point* point, double time);
+double regular_wave_get_elevation(struct Regular_wave* wave, 
+                                  struct Point* location, 
+                                  double time);
 
 #endif // REGULAR_WAVE_H
--- source/regular_wave.c
+++ source/regular_wave.c
@@ -1,19 +1,38 @@
 #include "regular_wave.h"
 #include "constants.h"
 
-double regular_wave_get_time_period(struct Regular_wave* wave)
+void regular_wave_init(struct Regular_wave* wave,
+                         double amplitude,
+                         double frequency,
+                         double phase,
+                         double direction)
 {
-  return (1.0/wave->frequency); // time in seconds
+  wave->amplitude = amplitude; 
+  wave->frequency = frequency;
+  wave->phase = phase;
+  wave->direction = direction;
+  wave->time_period = (1.0/frequency);
+  wave->wave_length = (G * wave->time_period * wave->time_period)/(2.0 * PI);
+  wave->wave_number = (2.0 * PI)/wave->wave_length;
 }
 
-double regular_wave_get_length(struct Regular_wave* wave)
+double Regular_wave_get_elevation(struct Regular_wave* wave,
+                                  struct Point* location,
+                                  double time)
 {
-  double t = regular_wave_get_time_period(wave);
-  return (G* t*t/ (2.0*PI)); // length in meter 
-}
-
-double regular_wave_get_wave_number(struct Regular_wave* wave)
-{
-  double l = regular_wave_get_length(wave);
-  return ((2.0*PI)/l);
-}
+  // elevation = amplitude * cos(A - B + phase)
+  // where:
+  // A = wave_number * (x * cos(direction) + y * sin(direction))
+  // B = 2 * PI * frequency * time
+  //
+  // NOTE:
+  // In the coordinate system that we use here, angular measurements are made 
+  // with respect to north which is represented by y-axis and not x-axis.
+  // Therefore the above formula needs to be modified as:
+  // A = wave_number * (x * sin(direction) + y * cos(direction))
+  
+  double A = wave->wave_number * (location->x * sin(wave->direction) + 
+                                  location->y * cos(wave->direction));
+  double B = 2.0 * PI * wave->frequency * time;
+  return wave->amplitude * cos(A - B + wave->phase);
+} 
