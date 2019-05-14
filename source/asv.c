b4a7bc57d57d978c305705ea5c444c4bb74364de
Toby Thomas <toby@tobythomas.tech>
2019-05-14 14:51:42 +0100

    Damping coefficients for linear motion.

--- source/asv.c
+++ source/asv.c
@@ -87,7 +87,82 @@ static void set_damping_matrix(struct Asv* asv)
   for(int i = 0; i < COUNT_DOF; ++i)
     asv->dynamics.C[i] = 0.0;
 
-  // TODO: Set damping coefficients.
+  // Ref: Recommended practices DNVGL-RP-N103 Modelling and analysis of marine
+  // operations. Edition July 2017. Appendix B Table B-1, B-2.
+  // Surge drag coefficient - assuming elliptical waterplane area
+  double B_L_ratio = asv->spec.B_wl/ asv->spec.L_wl;
+  if(B_L_ratio <= 0.125)
+  {
+    asv->dynamics.C[surge] = 0.22 * asv->spec.T;
+  }
+  else if (B_L_ratio > 0.125 && B_L_ratio <= 0.25)
+  {
+    asv->dynamics.C[surge] = 0.22 + (0.3-0.22)/(0.25-0.125)*(B_L_ratio - 0.125)*
+                             asv->spec.T;
+  }
+  else if (B_L_ratio > 0.25 && B_L_ratio <= 0.5)
+  {
+    asv->dynamics.C[surge] = 0.3+(0.6-0.3)/(0.5-0.25)*(B_L_ratio - 0.25) *
+                             asv->spec.T;
+  }
+  else if (B_L_ratio > 0.5 && B_L_ratio <= 1.0)
+  {
+    asv->dynamics.C[surge] = 0.6 + (1.0 - 0.6)/(1.0-0.5)*(B_L_ratio - 0.5) * 
+                             asv->spec.T;
+  }
+  else
+  {
+    asv->dynamics.C[surge] = 1.0 + (1.6 - 1.0)/(2.0-1.0)*(B_L_ratio - 1.0) *
+                             asv->spec.T;
+  }
+  
+  // Sway drag coefficient - if the vessel is cylindrical in shape then use the
+  // same value as for surge else consider it as a flat plat.
+  if(asv->spec.L_wl == asv->spec.B_wl)
+  {
+    asv->dynamics.C[sway] = asv->dynamics.C[surge];
+  }
+  else
+  {
+    double L_T_ratio = asv->spec.L_wl / asv->spec.T;
+    if(L_T_ratio <= 1.0)
+    {
+      asv->dynamics.C[sway] = 1.16;
+    }
+    else if (L_T_ratio > 1.0 && L_T_ratio <= 5.0)
+    {
+      asv->dynamics.C[sway] = 1.16 + (1.2 - 1.16)/(5.0 - 1.0)*(L_T_ratio - 1.0);
+    } 
+    else if (L_T_ratio > 5.0 && L_T_ratio <= 10.0)
+    {
+      asv->dynamics.C[sway] = 1.2 + (1.5- 1.2)/(10.0 - 5.0)*(L_T_ratio - 5.0);
+    }
+    else
+    {
+      asv->dynamics.C[sway] = 1.9;
+    }
+  }
+  
+  // Heave drag coefficient - consider it as flat plat perpendicular to flow.
+  double L_B_ratio = asv->spec.L_wl / asv->spec.B_wl;
+  if(L_B_ratio <= 1.0)
+  {
+    asv->dynamics.C[heave] = 1.16;
+  }
+  else if (L_B_ratio > 1.0 && L_B_ratio <= 5.0)
+  {
+    asv->dynamics.C[heave] = 1.16 + (1.2 - 1.16)/(5.0 - 1.0)*(L_B_ratio - 1.0);
+  } 
+  else if (L_B_ratio > 5.0 && L_B_ratio <= 10.0)
+  {
+    asv->dynamics.C[heave] = 1.2 + (1.5- 1.2)/(10.0 - 5.0)*(L_B_ratio - 5.0);
+  }
+  else
+  {
+    asv->dynamics.C[heave] = 1.9;
+  }
+
+  // roll, pitch and yaw drag coefficient assumed as 0.
 }
 
 // Method to set the stiffness matrix for the given asv object.
