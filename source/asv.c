14b6da37e07ce55623ceeddbe6c4fcc89f9bdc90
Toby Thomas <toby@tobythomas.tech>
2019-05-14 11:56:55 +0100

    Added mass changed to use the formula of a spheroid instead of a ellipsoid.

--- source/asv.c
+++ source/asv.c
@@ -17,7 +17,7 @@ static void set_mass_matrix(struct Asv* asv)
   double mass = asv->spec.disp * SEA_WATER_DENSITY;
 
   // Radius of gyration
-  // Assuming the ASV to homogeneous elliptical cylinder for the purpose of
+  // Assuming the ASV to be homogeneous elliptical cylinder for the purpose of
   // calculating the radius of gyrations.
   double a = asv->spec.L/2.0;
   double b = asv->spec.B/2.0;
@@ -28,52 +28,65 @@ static void set_mass_matrix(struct Asv* asv)
   
 
   // Added mass of the ASV
-  // ASV shape idealisation
+  // ASV shape idealisation:
   // For the purpose of calculating the added mass the shape of the ASV is
-  // assumed to be a semi ellipsoid.
+  // assumed to be a semi-spheroid, with the transverse cross section as a 
+  // semi-circle and the waterline as an ellipse.
   
-  // Added mass for ellipsoid. 
+  // Added mass for semi-spheroid. 
   // Ref: The complete expression for "added mass" of a rigid body moving in an
-  // ideal fluid. Frederick H Imlay. Page 15.
-  // Note: the formula given in the above reference is for a full ellipsoid but
-  // the shape that is assumed in this implementation is for a semi-ellipsoid.
-  // Therefore divide the values for added mass by 2.
-  double alpha_0 = 1.0;
-  double beta_0 = 1.0;
-  double gamma_0 = 1.0;
+  // ideal fluid. Frederick H Imlay. Page 16.
+  // Note: the formula given in the above reference is for a full spheroid but
+  // the shape that is assumed in this implementation is for a semi-spheroid.
+  // Therefore multiply added mass by 0.5.
   a = asv->spec.L_wl/2.0;
-  b = asv->spec.B_wl/2.0;
-  double c = asv->spec.T;
-  double added_mass_surge = 0.5 * (alpha_0/(2.0 - alpha_0)) *
+  // Find b such the volume of the semi-spheroid is equal to the displacement of
+  // the vessel.
+  b = sqrt((3.0/4.0) * (asv->spec.disp/(PI * a))); 
+
+  double e = sqrt(1.0 - pow(b/a, 2.0));
+  double alpha_0 = (2.0*(1 - e*e)/(e*e*e)) * (0.5*log10((1+e)/(1-e)) - e);
+  double beta_0 = (1.0/(e*e)) - ((1-e*e)/(2*e*e*e)) * log10((1+e)/(1-e));
+  
+  double added_mass_surge = fabs(
+                            0.5 * 
+                            (alpha_0/(2.0 - alpha_0)) * 
                             (4.0/3.0) * PI * 
                             SEA_WATER_DENSITY *
-                            a * b * c;
-  double added_mass_sway = 0.5 * (beta_0/(2.0 - beta_0)) * 
+                            a * b * b
+                            );
+  double added_mass_sway = fabs(
+                           0.5 * 
+                           (beta_0/(2.0 - beta_0)) *  
                            (4.0/3.0) * PI *
                            SEA_WATER_DENSITY * 
-                           a * b * c;
-  double added_mass_heave = 0.5 * (gamma_0/(2.0 - gamma_0)) * 
+                           a * b * b
+                           );
+  double added_mass_heave = added_mass_sway;
+  double added_mass_roll = 0.0;
+  double added_mass_pitch = fabs(
+                            0.5 *
+                            (1.0/5.0) * 
+                            pow(b*b - a*a, 2.0)*(alpha_0 - beta_0) / 
+                            (2.0*(b*b - a*a) + (b*b + a*a)*(beta_0 - alpha_0)) *
                             (4.0/3.0) * PI *
                             SEA_WATER_DENSITY * 
-                            a * b * c;
-  // Assuming added mass for roll, pitch and yaw to be 0.
+                            a * b * b
+                            );
+  double added_mass_yaw = added_mass_pitch;
 
   asv->dynamics.M[surge][surge] = mass + added_mass_surge;
   asv->dynamics.M[sway][sway]   = mass + added_mass_sway;
   asv->dynamics.M[yaw][yaw]     = mass + added_mass_heave;
 
   // Roll moment of inertia
-  asv->dynamics.M[roll][roll] = mass * r_roll*r_roll;
+  asv->dynamics.M[roll][roll] = mass * r_roll*r_roll + added_mass_roll;
   
   // Pitch moment of inertia
-  asv->dynamics.M[pitch][pitch] = mass * r_pitch*r_pitch;
+  asv->dynamics.M[pitch][pitch] = mass * r_pitch*r_pitch + added_mass_pitch;
 
   // Yaw moment of inertia
-  asv->dynamics.M[yaw][yaw] = mass * r_yaw*r_yaw;
-  asv->dynamics.M[surge][surge] += added_mass_surge;
-  asv->dynamics.M[sway][sway] += added_mass_sway;
-  asv->dynamics.M[heave][heave] += added_mass_heave;
-
+  asv->dynamics.M[yaw][yaw] = mass * r_yaw*r_yaw + added_mass_yaw;
 }
 
 // Method to set the damping matrix for the given asv object.
@@ -123,7 +136,9 @@ void asv_init(struct Asv* asv, struct Asv_specification* spec)
 {
   // Copy the specification
   asv->spec.L = spec->L;
+  asv->spec.L_wl = spec->L_wl;
   asv->spec.B = spec->B;
+  asv->spec.B_wl = spec->B_wl;
   asv->spec.D = spec->D;
   asv->spec.T = spec->T;
   asv->spec.max_speed = spec->max_speed;
