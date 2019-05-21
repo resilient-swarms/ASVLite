32d5633348ed59982ae3bd69e9353e51ffa32981
Toby Thomas <toby@tobythomas.tech>
2019-05-21 10:26:50 +0100

    Renamed Orientation to asv.h and renamed it to Asv_attitude.

--- include/geometry.h
+++ include/geometry.h
@@ -11,15 +11,4 @@ struct Point
   double z; // z coordinate in meter.
 };
 
-/**
- * Structure to represent the orientation in 3D space with respect to x, y, z 
- * axis.
- */
-struct Orientation
-{
-  double x; // Angle with x-axis in radian.
-  double y; // Angle with y-axis in radian.
-  double z; // Angle with z-axis in radian.
-}; 
-
 #endif // GEOMETRY_H
