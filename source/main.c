af092b0b948ba51d4532b944d35d3612bfd8e9ac
Toby Thomas <toby@tobythomas.tech>
2019-09-10 16:25:40 +0100

    Removed pid controller. Vehicle moves forward at full throttle.

--- source/main.c
+++ source/main.c
@@ -45,6 +45,12 @@ int main(int argc, char** argv)
   double run_time = 0.0; // initialise time
   int t = 0; // counter for time steps
   clock_t start, end;
+
+  // Propeller orientations are fixed.
+  struct Attitude propeller_orientation = (struct Attitude){0.0,0.0,0.0};
+
+  // Set destination
+  struct Point destination = (struct Point){100.0, 100.0, 0.0};
   
   // Start simulation
   for(double h = 0.0; h <= 15; h=h+0.5)
@@ -68,44 +74,15 @@ int main(int argc, char** argv)
                world.current);
     }
 
-    // Propeller orientations are fixed.
-    struct Attitude propeller_orientation = (struct Attitude){0.0,0.0,0.0};
-
-    // Initialise the PID controller
-    struct PID_controller controller;
-    pid_controller_init(&controller);
-    // PID controller set gain terms
-    double p_position = 1.0 * time_step_size/1000.0;
-    double i_position = 0.1 * time_step_size/1000.0;
-    double d_position = -10.0 * time_step_size/1000.0;
-    pid_controller_set_gains_position(&controller, 
-                                      p_position, i_position, d_position);
-    double p_heading = 1.0 * time_step_size/1000.0;
-    double i_heading = 0.1 * time_step_size/1000.0;
-    double d_heading = -10.0 * time_step_size/1000.0;
-    pid_controller_set_gains_heading(&controller, 
-                                     p_heading, i_heading, d_heading);
-
-    // Set destination
-    struct Point destination = (struct Point){100.0, 100.0, 0.0};
-    
     fprintf(stdout, 
       "\nStar simulation for significant wave height of {%f} m. \n", h);
     fprintf(stdout, "--> time step size = %f milli_seconds. \n", 
             time_step_size);
     
-    // Current error in position
-    double x1 = world.asv.cog_position.x;
-    double y1 = world.asv.cog_position.y;
-    double x2 = destination.x;
-    double y2 = destination.y;
-    double margin = 1.0; // acceptable error margin in m.
     // Start clock to measure time.
     start = clock();
-    while(sqrt(pow(x2-x1, 2.0) + pow(y2-y1, 2.0)) > margin)
+    while(world.asv.cog_position.x < destination.x)
     {
-      pid_controller_set_way_point(&controller, destination);
-
       // Get the wave elevation if wave is simulated.
       double wave_elevation = 0.0;
       if(world.wave)
@@ -115,25 +92,18 @@ int main(int argc, char** argv)
                                             run_time);
       }
 
-      // Inform PID controller of the current state.
-      pid_controller_set_current_state(&controller, 
-                                     world.asv.cog_position,
-                                     world.asv.attitude);
-      // PID controller estimate thrust to be applied on each propeller.
-      pid_controller_set_thrust(&controller);
-
       // Set the propeller thrust and orientation.
       asv_propeller_set_thrust(&world.asv.propellers[0], 
-                               controller.thrust_fore_ps,
+                               5.0,
                                propeller_orientation);
       asv_propeller_set_thrust(&world.asv.propellers[1], 
-                               controller.thrust_fore_sb,
+                               5.0,
                                propeller_orientation);
       asv_propeller_set_thrust(&world.asv.propellers[2], 
-                               controller.thrust_aft_ps,
+                               5.0,
                                propeller_orientation);
       asv_propeller_set_thrust(&world.asv.propellers[3], 
-                               controller.thrust_aft_sb,
+                               5.0,
                                propeller_orientation);
 
       // Get the asv dynamics for the current time step.
@@ -152,10 +122,6 @@ int main(int argc, char** argv)
         simulation_data[t].heel = world.asv.attitude.heel * 180.0/PI;
         simulation_data[t].trim = world.asv.attitude.trim * 180.0/PI;
         simulation_data[t].heel = world.asv.attitude.heading * 180.0/PI;
-        simulation_data[t].thrust_fore_ps = controller.thrust_fore_ps;
-        simulation_data[t].thrust_fore_sb = controller.thrust_fore_sb;
-        simulation_data[t].thrust_aft_ps = controller.thrust_aft_ps;
-        simulation_data[t].thrust_aft_sb = controller.thrust_aft_sb;
         ++t;
       }
       else
@@ -164,10 +130,6 @@ int main(int argc, char** argv)
         break;
         //return 0;
       }     
-
-      // reset variables for computing error
-      x1 = world.asv.cog_position.x;
-      y1 = world.asv.cog_position.y;
     }
     // Stop clock.
     end = clock();
@@ -205,10 +167,6 @@ int main(int argc, char** argv)
                "[06]heel(deg)  "
                "[07]trim(deg)  "
                "[08]heading(deg) " 
-               "[09]thrust_fore_ps(N) "
-               "[10]thrust_fore_sb(N) "
-               "[11]thrust_aft_ps(N)  "
-               "[12]thrust_aft_sb(N)  "
                "\n");
     for(int i = 0; i < t; ++i)
     {
@@ -220,11 +178,7 @@ int main(int argc, char** argv)
               simulation_data[i].cog_z, 
               simulation_data[i].heel, 
               simulation_data[i].trim, 
-              simulation_data[i].heading,
-              simulation_data[i].thrust_fore_ps, 
-              simulation_data[i].thrust_fore_sb,
-              simulation_data[i].thrust_aft_ps,
-              simulation_data[i].thrust_aft_sb);
+              simulation_data[i].heading);
     }
 
     fprintf(stdout, "--> task duration = %f seconds. \n", run_time);
