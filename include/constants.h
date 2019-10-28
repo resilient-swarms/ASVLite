153d7e4d59fd953adb8dc46c31858d34930259fb
Toby Thomas <toby@tobythomas.tech>
2019-10-28 17:11:08 +0000

    Minor edit.

--- include/constants.h
+++ include/constants.h
@@ -8,7 +8,7 @@
 #include <math.h>
 
 #define PI M_PI
-#define G 9.81 /* Acceleration due to gravity in m/s */ 
+#define G 9.81 /* Acceleration due to gravity in m/s2 */ 
 #define SEA_WATER_DENSITY 1025 /* Sea water density in Kg/m3 */
 #define AIR_DENSITY 1.2 /* Kg/m3 */
 
