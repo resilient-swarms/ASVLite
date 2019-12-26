dd49e3cd2423cb668c6cae5c3629c85bf1e1ae73
Toby Thomas <toby@tobythomas.tech>
2019-12-26 16:04:39 +0000

    Read thrust.csv file and set thrust force for each time step.

--- source/main_regular_wave.c
+++ source/main_regular_wave.c
@@ -3,16 +3,31 @@
 #include <string.h>
 #include <time.h>
 #include <io.h>
+#include <math.h>
 #include "asv.h"
+
+#define MAX_LINE_LENGTH 1000
+
+// Function to read field from a csv file
+void get_thrust_vec(char* line, double* thrust_vec)
+{
+  int i = 0; 
+  char* column = strtok(line, ",");
+  for(int i = 0; column != NULL && i < 4; ++i)
+  {
+    thrust_vec[i] = atof(column);
+    column = strtok(NULL, ",");
+  }
+}
 
 int main(int argc, char** argv)
 {
-  if(argc != 7)
+  if(argc != 8)
   {
     fprintf(stderr, 
       "Error. " 
-      "Usage: %s in_file out_file wave_ht(m) wave_heading(deg) frequency(Hz) "
-      "phase_lag(deg).\n", 
+      "Usage: %s in_file out_file thrust_file wave_ht(m) wave_heading(deg) "
+      "frequency(Hz) phase_lag(deg).\n", 
       argv[0]);
     return 1;
   }
@@ -20,10 +35,11 @@ int main(int argc, char** argv)
   int rand_seed = 1;
   char* in_file = argv[1];
   char* out_file = argv[2];
-  sscanf(argv[3], "%lf", &wave_ht);
-  sscanf(argv[4], "%lf", &wave_heading);
-  sscanf(argv[5], "%lf", &frequency);
-  sscanf(argv[6], "%lf", &phase_lag);
+  char* thrust_file_name = argv[3];
+  sscanf(argv[4], "%lf", &wave_ht);
+  sscanf(argv[5], "%lf", &wave_heading);
+  sscanf(argv[6], "%lf", &frequency);
+  sscanf(argv[7], "%lf", &phase_lag);
 
   // Init vehicle and waypoints
   struct Asv asv;
@@ -49,9 +65,27 @@ int main(int argc, char** argv)
   double simulation_time = 0.0;
   clock_t start, end;
   start = clock();
-  for(int i = 0; i < waypoints.count; ++i)
+  // Read thrust input file
+  FILE* thrust_file = fopen(thrust_file_name, "r");
+  if(thrust_file == NULL)
   {
-    for(;;++t) // break when reached the waypoint
+    fprintf(stderr, "ERROR: could not open thrust file.");
+    exit(1);
+  }
+  char thrust_file_line[MAX_LINE_LENGTH];
+  while(fgets(thrust_file_line, MAX_LINE_LENGTH, thrust_file) != NULL)
+  {
+    // set propeller thrust and direction
+    // TODO: Set propeller force.
+    double thrust_vec[4];
+    get_thrust_vec(thrust_file_line, thrust_vec); 
+    asv.propellers[0].thrust = sqrt(thrust_vec[2] * thrust_vec[2] +
+                                    thrust_vec[3] * thrust_vec[3]); //N
+    // TODO: time when the propeller should be applied.
+    double t1 = thrust_vec[1];
+    asv.propellers[0].orientation = (struct Dimensions){0.0, 0.0, 0.0};
+    
+    for(;t<t1;++t) 
     {
       // check buffer limit reached
       if(t >= OUTPUT_BUFFER_SIZE)
@@ -69,54 +103,35 @@ int main(int argc, char** argv)
 	      exit(1);
       }
 
-      // check if reached destination
-      double proximity_margin = 10.0; // target proximity to waypoint
-      double x = asv.cog_position.x - waypoints.points[i].x;
-      double y = asv.cog_position.y - waypoints.points[i].y;
-      double distance = sqrt(x*x + y*y);
-      if(distance <= proximity_margin)
+      // compute time
+      time = t * asv.dynamics.time_step_size; //sec
+
+      // compute new position and attitude.
+      asv_compute_dynamics(&asv, time);
+
+      // Also compute the wave elevation. 
+      double wave_elevation = 0.0;
+      if(asv.wave_type == regular_wave)
       {
-        break;
+        struct Dimensions wave_probe = {0, 0, 0};
+        wave_elevation = regular_wave_get_elevation(&asv.regular_wave, 
+                                                    &wave_probe, time);
       }
-      else
-      {
-        // compute time
-        time = t * asv.dynamics.time_step_size; //sec
 
-        // set propeller thrust and direction
-        for(int p = 0; p < asv.count_propellers; ++p)
-        {
-          asv.propellers[p].thrust = 0.0; //N
-          asv.propellers[p].orientation = (struct Dimensions){0.0, 0.0, 0.0};
-        } 
-
-        // compute new position and attitude.
-        asv_compute_dynamics(&asv, time);
-
-        // Also compute the wave elevation. 
-        double wave_elevation = 0.0;
-        if(asv.wave_type == regular_wave)
-        {
-          struct Dimensions wave_probe = {0, 0, 0};
-          wave_elevation = regular_wave_get_elevation(&asv.regular_wave, 
-                                              &wave_probe, time);
-        }
-
-        // save simulated data to buffer. 
-        buffer[t].sig_wave_ht = asv.wave.significant_wave_height;
-        buffer[t].wave_heading = asv.wave.heading * 180.0/PI;
-        buffer[t].random_number_seed = rand_seed;
-        buffer[t].time = time;
-        buffer[t].wave_elevation = wave_elevation;
-        buffer[t].cog_x = asv.cog_position.x;
-        buffer[t].cog_y = asv.cog_position.y;
-        buffer[t].cog_z = asv.cog_position.z - (asv.spec.cog.z - asv.spec.T);
-        buffer[t].heel = asv.attitude.x * 180.0/PI;
-        buffer[t].trim = asv.attitude.y * 180.0/PI;
-        buffer[t].heading = asv.attitude.z * 180.0/PI;
-        buffer[t].surge_velocity = asv.dynamics.V[surge];
-        buffer[t].surge_acceleration = asv.dynamics.A[surge];
-      }
+      // save simulated data to buffer. 
+      buffer[t].sig_wave_ht = asv.wave.significant_wave_height;
+      buffer[t].wave_heading = asv.wave.heading * 180.0/PI;
+      buffer[t].random_number_seed = rand_seed;
+      buffer[t].time = time;
+      buffer[t].wave_elevation = wave_elevation;
+      buffer[t].cog_x = asv.cog_position.x;
+      buffer[t].cog_y = asv.cog_position.y;
+      buffer[t].cog_z = asv.cog_position.z - (asv.spec.cog.z - asv.spec.T);
+      buffer[t].heel = asv.attitude.x * 180.0/PI;
+      buffer[t].trim = asv.attitude.y * 180.0/PI;
+      buffer[t].heading = asv.attitude.z * 180.0/PI;
+      buffer[t].surge_velocity = asv.dynamics.V[surge];
+      buffer[t].surge_acceleration = asv.dynamics.A[surge];
     }
   }
   end = clock();
