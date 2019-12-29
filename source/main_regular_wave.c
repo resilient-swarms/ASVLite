473143027170f7869dfd6d4990ff87f569649d6c
Toby Thomas <toby@tobythomas.tech>
2019-12-29 01:09:53 +0000

    Simulate based on epoch time

--- source/main_regular_wave.c
+++ source/main_regular_wave.c
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <io.h>
 #include <math.h>
+#include <limits.h>
 #include "asv.h"
 
 #define MAX_LINE_LENGTH 1000
@@ -13,7 +14,7 @@ void get_thrust_vec(char* line, double* thrust_vec)
 {
   int i = 0; 
   char* column = strtok(line, ",");
-  for(int i = 0; column != NULL && i < 4; ++i)
+  for(int i = 0; column != NULL && i < 3; ++i)
   {
     thrust_vec[i] = atof(column);
     column = strtok(NULL, ",");
@@ -60,7 +61,8 @@ int main(int argc, char** argv)
   asv_init(&asv);
 
   // Simulate
-  int t = 0; // counter for time
+  unsigned long t0 = ULONG_MAX; // counter for time
+  unsigned long t = ULONG_MAX; // counter for time
   double time = 0.0;
   double simulation_time = 0.0;
   clock_t start, end;
@@ -76,20 +78,26 @@ int main(int argc, char** argv)
   while(fgets(thrust_file_line, MAX_LINE_LENGTH, thrust_file) != NULL)
   {
     // Set propeller force.
-    double thrust_vec[4];
+    double thrust_vec[3];
     get_thrust_vec(thrust_file_line, thrust_vec); 
     // time when the propeller should be applied.
-    double t1 = thrust_vec[1];
+    unsigned long epoch = (unsigned long)(thrust_vec[0]*1000.0); // milli-sec
+    // Set the simulation start time as equal to start epoch time of thrust file
+    if(t > epoch)
+    {
+      t0 = epoch;
+      t = epoch; 
+    }
     // set propeller thrust and direction
-    asv.propellers[0].thrust = thrust_vec[2];
-    asv.propellers[1].thrust = thrust_vec[3];
-    asv.propellers[0].orientation = (struct Dimensions){0.0, 0.0, 0.0};
+    asv.propellers[0].thrust = thrust_vec[1];
+    asv.propellers[1].thrust = thrust_vec[2];
+    asv.propellers[0].orientation = (struct Dimensions){0.0, 0.0, PI};
     asv.propellers[1].orientation = (struct Dimensions){0.0, 0.0, PI/2.0};
     
-    for(;t<=t1;++t) 
+    for(;t<=epoch;++t) 
     {
       // check buffer limit reached
-      if(t >= OUTPUT_BUFFER_SIZE)
+      if((t-t0) >= OUTPUT_BUFFER_SIZE)
       {
         fprintf(stderr, "ERROR: output buffer exceeded.\n");
         // write output to file
@@ -105,7 +113,7 @@ int main(int argc, char** argv)
       }
 
       // compute time
-      time = t * asv.dynamics.time_step_size; //sec
+      time = (t - t0) * asv.dynamics.time_step_size; //sec
 
       // compute new position and attitude.
       asv_compute_dynamics(&asv, time);
@@ -120,21 +128,22 @@ int main(int argc, char** argv)
       }
 
       // save simulated data to buffer. 
-      buffer[t].sig_wave_ht = asv.wave.significant_wave_height;
-      buffer[t].wave_heading = asv.wave.heading * 180.0/PI;
-      buffer[t].random_number_seed = rand_seed;
-      buffer[t].time = time;
-      buffer[t].wave_elevation = wave_elevation;
-      buffer[t].cog_x = asv.cog_position.x;
-      buffer[t].cog_y = asv.cog_position.y;
-      buffer[t].cog_z = asv.cog_position.z - (asv.spec.cog.z - asv.spec.T);
-      buffer[t].heel = asv.attitude.x * 180.0/PI;
-      buffer[t].trim = asv.attitude.y * 180.0/PI;
-      buffer[t].heading = asv.attitude.z * 180.0/PI;
-      buffer[t].surge_velocity = asv.dynamics.V[surge];
-      buffer[t].surge_acceleration = asv.dynamics.A[surge];
-      buffer[t].F_surge = thrust_vec[2];
-      buffer[t].F_sway = thrust_vec[3];
+      int i = t - t0;
+      buffer[i].sig_wave_ht = asv.wave.significant_wave_height;
+      buffer[i].wave_heading = asv.wave.heading * 180.0/PI;
+      buffer[i].random_number_seed = rand_seed;
+      buffer[i].time = (t0/1000.0) + time;
+      buffer[i].wave_elevation = wave_elevation;
+      buffer[i].cog_x = asv.cog_position.x;
+      buffer[i].cog_y = asv.cog_position.y;
+      buffer[i].cog_z = asv.cog_position.z - (asv.spec.cog.z - asv.spec.T);
+      buffer[i].heel = asv.attitude.x * 180.0/PI;
+      buffer[i].trim = asv.attitude.y * 180.0/PI;
+      buffer[i].heading = asv.attitude.z * 180.0/PI;
+      buffer[i].surge_velocity = asv.dynamics.V[surge];
+      buffer[i].surge_acceleration = asv.dynamics.A[surge];
+      buffer[i].F_surge = thrust_vec[1];
+      buffer[i].F_sway = thrust_vec[2];
     }
   }
   end = clock();
