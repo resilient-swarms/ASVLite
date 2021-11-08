81352eb903c25d6c6691a6001fc9f74c1d9749fc
Toby Thomas <toby@tobythomas.tech>
2021-11-08 00:29:55 +0000

    renamed file.

---
+++ controllers/simulate_tracking_cyclone.py
@@ -0,0 +1,165 @@
+import sys
+import toml
+import collections
+import math
+import csv
+from dataclasses import dataclass
+sys.path.insert(0, "../wrapper")
+from wave import Wave
+from asv import Asv_propeller, Asv_specification, Asv_dynamics, Asv
+from geometry import Dimensions
+from rudder_controller_pid import Rudder_controller
+from wave_data import Wave_data
+from storm_track import Storm_track
+from datetime import datetime, timedelta
+from tqdm import tqdm
+
+@dataclass
+class _Simulation_object:
+    '''
+    A class to hold the simulation data related to a single ASV.
+    '''
+    id: int
+    wave: Wave
+    asv: Asv
+    waypoints: list
+    current_waypoint_index: int
+    simulation_data : list # Hold the following simulation data - [cog_x, cog_y, cog_z, asv_heading, v_surge].
+    controller : Rudder_controller
+
+class Simulation:
+    '''
+    Class to perform the simulation of vehicle and waves.
+    '''
+    def __init__(self, asv_input_file, nc_file_path, storm_track, simulation_start_time, simulation_end_time):
+        toml_file = asv_input_file
+        # Wave data
+        self.wave_data = Wave_data(nc_file_path)
+        # Cyclone track
+        self.storm_track = Storm_track(storm_track)
+        # Get the time range of the simulation.
+        self.simulation_start_time = simulation_start_time
+        self.simulation_end_time = simulation_end_time
+        # Create ASVs 
+        self.asvs = [] # collection of simulation objects, ie asvs
+        toml_data = toml.load(toml_file)
+        if not "asv" in toml_data:
+            raise ValueError("Table [[asv]] missing in input file.")
+        for item in toml_data["asv"]:
+            asv = Asv()
+            id = item["id"]
+            asv.spec.L_wl = item["L_wl"]
+            asv.spec.B_wl = item["B_wl"]
+            asv.spec.D = item["D"]
+            asv.spec.T = item["T"]
+            asv.spec.max_speed = item["max_speed"]
+            asv.spec.disp = item["displacement"]
+            asv.spec.r_roll  = item["radius_of_gyration"][0]
+            asv.spec.r_pitch = item["radius_of_gyration"][1]
+            asv.spec.r_yaw   = item["radius_of_gyration"][2]
+            asv.spec.cog = Dimensions(*item["cog"])
+            asv.origin_position = Dimensions(*item["asv_position"])
+            asv.attitude = Dimensions(*[angle * math.pi/180.0 for angle in item["asv_attitude"]])
+            thrusters = item["thrusters"]
+            for i in range(len(thrusters)):
+                asv.propellers[i] = Asv_propeller()
+                asv.propellers[i].position = Dimensions(*thrusters[i])  
+            _waypoints = item["waypoints"]
+            waypoints = []
+            for waypoint in _waypoints:
+                waypoints.append(Dimensions(*waypoint))
+            #Create wave
+            start_latitude = asv.origin_position.x
+            start_longitude = asv.origin_position.y
+            wave_hs, wave_dp = self.wave_data.get_wave_data_at(start_latitude, start_longitude, self.simulation_start_time)
+            rand_seed = 1
+            wave = Wave(wave_hs, wave_dp, rand_seed)
+            self.asvs.append(_Simulation_object(id, wave, asv, waypoints, 0, [], Rudder_controller(asv.spec, [25,1,9])))
+        if "clock" in toml_data:
+            self.time_step_size = toml_data["clock"]["time_step_size"] # millisec
+        else:
+            self.time_step_size = 40 # millisec
+        for item in self.asvs:
+            # Set the clock
+            item.asv.dynamics.time_step_size = self.time_step_size/1000.0 # sec
+            item.asv.dynamics.time = 0.0 
+            # Initialise the ASV
+            item.asv.init(item.wave)      
+    
+    def run(self):
+        record_asv_path = [] # To record the asv path to a text file
+        record_distance_to_storm = [] # To record the distance between asv and storm to a text file
+        record_hs = [] # To record the significant wave height experianced by the asv to a text file
+        time = self.simulation_start_time
+        time_step_size = self.time_step_size/1000.0 # sec
+        times = [] # List of time steps to simulate
+        while time <= self.simulation_end_time:
+            times.append(time)           
+            time = time + timedelta(seconds=time_step_size)
+        for current_time in tqdm(times):
+            # Find the position of the storm for the current time
+            current_time = current_time + timedelta(seconds=time_step_size)
+            cyclone_eye = self.storm_track.get_eye_location(current_time)
+            for item in self.asvs:
+                # Get the distance to the centre of the storm
+                # Ref: https://www.movable-type.co.uk/scripts/latlong.html
+                lat1  = item.asv.cog_position.x * math.pi/180.0         
+                long1 = item.asv.cog_position.y * math.pi/180.0
+                lat2  = item.waypoints[0].x * math.pi/180.0
+                long2 = item.waypoints[0].y * math.pi/180.0
+                d_lat = (lat2 - lat1)
+                d_long = (long2 - long1) 
+                a = (math.sin(d_lat/2.0))**2 + math.cos(lat2)*math.cos(lat1)*(math.sin(d_long/2.0))**2
+                c = 2.0 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+                R = 6378000.0
+                distance = R*c / 1000.0 # Km
+                record_distance_to_storm.append([distance])
+                # Get the sea state
+                current_latitude = item.asv.cog_position.x
+                current_longitude = item.asv.cog_position.y
+                new_hs, new_dp = self.wave_data.get_wave_data_at(current_latitude, current_longitude, current_time)
+                # Compare the sea state with the current sea state
+                current_hs = item.wave.significant_wave_height
+                current_dp = item.wave.heading
+                record_hs.append([current_hs])
+                is_sea_state_same = (float(new_hs) == float(current_hs) and float(new_dp == current_dp))
+                # If the sea state has changed then, set the new sea state in the asv object
+                if not is_sea_state_same:
+                    rand_seed = 1
+                    #print(wave_hs)
+                    item.wave = Wave(new_hs, new_dp, rand_seed)
+                    item.asv.set_sea_state(item.wave)
+                # TODO: Update waypoint if required
+                item.waypoints[0] = Dimensions(*cyclone_eye)
+                # Set rudder angle
+                using_earth_coordinate_system = True
+                rudder_angle = item.controller.get_rudder_angle(item.asv, item.waypoints[0], using_earth_coordinate_system)
+                # Compute the dynamics for the current time step
+                seconds = (current_time - self.simulation_start_time).total_seconds()
+                item.asv.compute_dynamics(rudder_angle, seconds)
+                # Save simulation data
+                item.simulation_data.append([item.asv.cog_position.x, 
+                                            item.asv.cog_position.y, 
+                                            item.asv.cog_position.z, 
+                                            item.asv.attitude.z, 
+                                            item.asv.dynamics.V[0]])
+                record_asv_path.append([seconds, current_time, item.asv.cog_position.x, item.asv.cog_position.y])
+        # Write the data to the corresponding files
+        records = {"./path.txt": record_asv_path, "./distance.txt":record_distance_to_storm, "./hs.txt":record_hs}
+        for file, record in records.items():
+            f = open(file, "w")  
+            writer = csv.writer(f, delimiter=" ")
+            writer.writerows(record)
+            f.close()
+
+if __name__ == '__main__':   
+    asv_input_file = "./sample_files/katrina/wave_glider"
+    nc_file_path = "./sample_files/katrina/wave_data.nc"
+    storm_track = "./sample_files/katrina/track.csv"
+    simulation_start_time = datetime(2005, 8, 26, 9)
+    simulation_end_time = datetime(2005, 8, 30, 1)
+    simulation = Simulation(asv_input_file, nc_file_path, storm_track, simulation_start_time, simulation_end_time)
+    simulation.run()
+
+# p "../sample_files/cyclone_path.txt" u 2:1 w l, "../sample_files/world_10m.txt" u 1:2 w l, "path_01.txt" u 4:3 w l, "path_02.txt" u 4:3 w l, "path_03.txt" u 4:3 w l, "path_04.txt" u 4:3 w l, "path_05.txt" u 4:3 w l, "path_06.txt" u 4:3 w l, "path_07.txt" u 4:3 w l
+# p "distance_01.txt" w l, "distance_02.txt" w l, "distance_03.txt" w l, "distance_04.txt" w l, "distance_05.txt" w l, "distance_06.txt" w l, "distance_07.txt" w l
