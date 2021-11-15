f61634888aaf93ba0ad847b377e04f90972381f9
Toby Thomas <toby@tobythomas.tech>
2021-11-15 23:44:05 +0000

    Defined constants.

---
+++ ASVLite-Rust/src/constants.rs
@@ -0,0 +1,9 @@
+
+/// Math constant PI.
+pub const PI: f64 = std::f64::consts::PI;
+
+/// Acceleration due to gravity in m/s2.
+pub const G: f64 = 9.81;
+
+/// Sea water density in Kg/m3.
+pub const SEA_WATER_DENSITY: f64 = 1025.0;
