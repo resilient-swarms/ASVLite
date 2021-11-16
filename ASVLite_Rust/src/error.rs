cacca71f8a05d6fbc65c0def4fcc644f07919424
Toby Thomas <toby@tobythomas.tech>
2021-11-16 00:13:57 +0000

    Implemented ValueError.

---
+++ ASVLite_Rust/src/error.rs
@@ -0,0 +1,28 @@
+use std::fmt;
+
+/// Defining ValueError type. 
+#[derive(Debug, Clone)]
+struct ValueError {
+    message: String,
+}
+
+impl ValueError {
+    /// Constructor.
+    pub fn new(message: &str) -> ValueError {
+        ValueError{message: String::from(message)}
+    }
+}
+
+impl fmt::Display for ValueError {
+    /// Display error.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+#[test]
+fn value_error() {
+    let error_message = "Unit test for error creation.";
+    let test_error = ValueError::new(error_message);
+    assert_eq!(format!("{}", test_error), error_message);
+}
