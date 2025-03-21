70b922626cde619786470a5c7e33d86ce5e056b6
Toby Thomas <toby@tobythomas.tech>
2025-03-21 05:54:51 +0000

    Measure runtime performance.

--- cpp_current/source/main.cpp
+++ cpp_current/source/main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <filesystem>
 #include <stdexcept>
+#include <chrono>
 
 using namespace ASVLite;
 
@@ -12,10 +13,7 @@ int main() {
     if (!std::filesystem::exists(results_dir)) {
         std::filesystem::create_directory(results_dir);
     }
-    else {
-        std::cout << results_dir << " already exists." << std::endl;
-        return 1;
-    }
+    
     std::filesystem::path result_file_path = results_dir/("simulation_data.csv");
     std::ofstream file(result_file_path); 
 
@@ -43,42 +41,48 @@ int main() {
     file << "x,y,z,submersion_depth,F_wave,F_drag,F_restoring,F_thrust,F_net,M_surge,M_sway,M_heave,I_roll,I_pitch,I_yaw,a_surge,a_sway,a_heave,a_roll,a_pitch,a_yaw,v_surge,v_swary,v_heave,v_roll,v_pitch,v_yaw,roll,pitch,yaw\n";
     const double simulation_duration = 60 * 60; // sec
     int i = 0;
+    auto start = std::chrono::high_resolution_clock::now();
     while(asv.get_time() < simulation_duration) {
         ++i;
         auto [thrust_position, thrust_magnitude] = get_wave_glider_thrust(asv, 0.0, sea_surface.significant_wave_height);
         asv.step_simulation(thrust_position, thrust_magnitude);
         
-        file<< asv.get_position().keys.x << "," 
-            << asv.get_position().keys.y << "," 
-            << asv.get_position().keys.z << ","
-            << asv.get_submersion_depth() << ","
-            << asv.get_wave_force().keys.heave << ","
-            << asv.get_drag_force().keys.heave << ","
-            << asv.get_restoring_force().keys.heave << ","
-            << asv.get_propulsive_thrust().keys.heave << ","
-            << asv.get_net_force().keys.heave << ","
-            << asv.get_mass().keys.surge << ","
-            << asv.get_mass().keys.sway << ","
-            << asv.get_mass().keys.heave << ","
-            << asv.get_mass().keys.roll << ","
-            << asv.get_mass().keys.pitch << ","
-            << asv.get_mass().keys.yaw << ","
-            << asv.get_acceleration().keys.surge << ","
-            << asv.get_acceleration().keys.sway << ","
-            << asv.get_acceleration().keys.heave << ","
-            << asv.get_acceleration().keys.roll << ","
-            << asv.get_acceleration().keys.pitch << ","
-            << asv.get_acceleration().keys.yaw << ","
-            << asv.get_velocity().keys.surge << ","
-            << asv.get_velocity().keys.sway << ","
-            << asv.get_velocity().keys.heave << ","
-            << asv.get_velocity().keys.roll << ","
-            << asv.get_velocity().keys.pitch << ","
-            << asv.get_velocity().keys.yaw << ","
-            << asv.get_attitude().keys.x * 180.0/M_PI << ","
-            << asv.get_attitude().keys.y * 180.0/M_PI << ","
-            << asv.get_attitude().keys.z * 180.0/M_PI << "\n";
+        // file<< asv.get_position().keys.x << "," 
+        //     << asv.get_position().keys.y << "," 
+        //     << asv.get_position().keys.z << ","
+        //     << asv.get_submersion_depth() << ","
+        //     << asv.get_wave_force().keys.heave << ","
+        //     << asv.get_drag_force().keys.heave << ","
+        //     << asv.get_restoring_force().keys.heave << ","
+        //     << asv.get_propulsive_thrust().keys.heave << ","
+        //     << asv.get_net_force().keys.heave << ","
+        //     << asv.get_mass().keys.surge << ","
+        //     << asv.get_mass().keys.sway << ","
+        //     << asv.get_mass().keys.heave << ","
+        //     << asv.get_mass().keys.roll << ","
+        //     << asv.get_mass().keys.pitch << ","
+        //     << asv.get_mass().keys.yaw << ","
+        //     << asv.get_acceleration().keys.surge << ","
+        //     << asv.get_acceleration().keys.sway << ","
+        //     << asv.get_acceleration().keys.heave << ","
+        //     << asv.get_acceleration().keys.roll << ","
+        //     << asv.get_acceleration().keys.pitch << ","
+        //     << asv.get_acceleration().keys.yaw << ","
+        //     << asv.get_velocity().keys.surge << ","
+        //     << asv.get_velocity().keys.sway << ","
+        //     << asv.get_velocity().keys.heave << ","
+        //     << asv.get_velocity().keys.roll << ","
+        //     << asv.get_velocity().keys.pitch << ","
+        //     << asv.get_velocity().keys.yaw << ","
+        //     << asv.get_attitude().keys.x * 180.0/M_PI << ","
+        //     << asv.get_attitude().keys.y * 180.0/M_PI << ","
+        //     << asv.get_attitude().keys.z * 180.0/M_PI << "\n";
     }
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+    std::cout << "Simulation completed in " << duration.count()/1000'000.0 << " seconds." << std::endl;
+    std::cout << "Simulation speed " << simulation_duration/duration.count()*1000'000.0 << " X realtime speed." << std::endl;
 
     return 0;
 }
